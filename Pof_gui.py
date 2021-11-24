f4198093d4da1d13705a03d62b4562a514dd5707
Pablion <36828324+Pablion@users.noreply.github.com>
2021-11-24 10:41:04 +0100

    chore: modularize GUI

---
+++ Pof_gui.py
@@ -0,0 +1,30 @@
+import eel
+
+global GUI_enabled
+GUI_enabled = False
+
+
+def check_GUI_enabled(func):
+    if GUI_enabled:
+        return func
+    else:
+        return lambda msg: print(
+            "GUI is not enabled or not initiated yet, printing with Python console:"
+            + msg
+        )
+
+
+def init_GUI():
+    global GUI_enabled
+    GUI_enabled = True
+    eel.init("web")
+    eel.start("app.html", size=(800, 800))
+
+
+@check_GUI_enabled
+def print_log(message):
+    eel.addText(message)
+
+
+def expose(func):
+    return eel.expose(func)
